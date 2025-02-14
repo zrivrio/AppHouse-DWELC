b60a365e2ed58b8bfcd33a3d9e82263fe7da38ca
Zahira <zrivrio2103@gmail.com>
2025-02-14 20:41:54 +0100

    parte estra realizada

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
+import { Housinglocation } from '../../models/housinglocation';
+import { HousingService } from '../../services/housing.service';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { Housinglocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
-import { HousingService } from '../housing.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent, CommonModule],
+  imports: [HousingLocationComponent, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'], // Fixed typo
 })
