356c5d52a3c1ab78d5fd98807fab8e56d586b976
zrivrio <zrivrio2103@gmail.com>
2025-02-18 12:54:23 +0100

    comentarios

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -5,15 +5,20 @@ import { Housinglocation } from '../models/housinglocation';
   providedIn: 'root',
 })
 export class HousingService {
+
+  //Funcion en la que obtiene del json el array de las propiedades
   url = 'http://localhost:3000/locations';
   async getAllHousingLocations(): Promise<Housinglocation[]> {
     const data = await fetch(this.url);
     return (await data.json()) ?? [];
   }
+  //Funcion en la que obtienes una casa por su id
   async getHousingLocationById(id: number): Promise<Housinglocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
     return (await data.json()) ?? {};
   }
+
+  //Funcion en la que muestrar por consola los datos que te ha metido el usuario en el formulario
   submitApplication(firstName: string, lastName: string, email: string) {
     // tslint:disable-next-line
     console.log(firstName, lastName, email);
