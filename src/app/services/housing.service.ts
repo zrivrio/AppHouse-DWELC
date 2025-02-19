ab07f6575739f0170798bbb7007334c0db0f8b13
zrivrio <zrivrio2103@gmail.com>
2025-02-19 11:10:32 +0100

    examen

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import {Injectable } from '@angular/core';
 import { Housinglocation } from '../models/housinglocation';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HousingService {
+  //constructor( private http : HttpClient){}
 
   //Funcion en la que obtiene del json el array de las propiedades
   url = 'http://localhost:3000/locations';
@@ -22,5 +24,12 @@ export class HousingService {
   submitApplication(firstName: string, lastName: string, email: string) {
     // tslint:disable-next-line
     console.log(firstName, lastName, email);
+  }
+
+  addEvento(casa: Housinglocation) { 
+    this.getAllHousingLocations().then((housingLocationList: Housinglocation[]) =>{
+      housingLocationList.push(casa);
+    })
+    console.log(casa);
   }
 }
